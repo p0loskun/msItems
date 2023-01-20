e5b1f5c3dec8a763630f967e495c61e805f34082
Poloskun_ <nikongricisin339@gmail.com>
2023-01-20 22:23:37 +0300

    now the player cannot add a card box to the shulker

--- src/main/java/com/github/minersstudios/msitems/listeners/mechanic/CardBoxMechanic.java
+++ src/main/java/com/github/minersstudios/msitems/listeners/mechanic/CardBoxMechanic.java
@@ -8,6 +8,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.event.inventory.InventoryMoveItemEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BundleMeta;
 import org.jetbrains.annotations.NotNull;
@@ -19,11 +23,43 @@ import java.util.List;
 public class CardBoxMechanic implements Listener {
 
 	@EventHandler(priority = EventPriority.MONITOR)
+	public void onInventoryMoveItem(@NotNull InventoryMoveItemEvent event) {
+		if (
+				event.getDestination().getType() == InventoryType.SHULKER_BOX
+				&& ItemUtils.getCustomItem(event.getItem()) instanceof CardsBicycle
+		) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onInventoryDrag(@NotNull InventoryDragEvent event) {
+		if (
+				event.getInventory().getType() == InventoryType.SHULKER_BOX
+				&& ItemUtils.getCustomItem(event.getOldCursor()) instanceof CardsBicycle
+		) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onInventoryClick(@NotNull InventoryClickEvent event) {
-		if (!event.isRightClick()) return;
 		ItemStack cursorItem = event.getCursor();
 		ItemStack currentItem = event.getCurrentItem();
-		if (cursorItem == null || currentItem == null) return;
+		Inventory clickedInventory = event.getClickedInventory();
+
+		if (
+				(clickedInventory != null
+				&& clickedInventory.getType() == InventoryType.SHULKER_BOX
+				&& ItemUtils.getCustomItem(cursorItem) instanceof CardsBicycle)
+				|| (event.isShiftClick()
+				&& event.getWhoClicked().getOpenInventory().getType() == InventoryType.SHULKER_BOX
+				&& ItemUtils.getCustomItem(currentItem) instanceof CardsBicycle)
+		) {
+			event.setCancelled(true);
+		}
+
+		if (cursorItem == null || currentItem == null || !event.isRightClick()) return;
 		if (
 				!cursorItem.getType().isAir()
 				&& ItemUtils.getCustomItem(currentItem) instanceof CardsBicycle
